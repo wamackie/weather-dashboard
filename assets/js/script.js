7a6983382dff1a047b042241f7b9ab1a07b168b3
Wamackie <wamackie8456@gmail.com>
2021-07-31 05:13:16 -0400

    neatened code

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,9 +37,6 @@ function getApi() {
 
         var currentHum = data.main.humidity
         currentHumEl.textContent = currentHum
-        
-        // var currentUvi = data.current.uvi;
-        // currentUviEl.textContent = currentUvi
 
         function secondCall(lat,lon){
           var lat = data.coord.lat
@@ -60,19 +57,10 @@ function getApi() {
                 var currentUvi = data.current.uvi
                 currentUviEl.textContent = currentUvi
 
-              //var uvi = data.
           console.log(lat,lon)
         })
         }
         secondCall()
-
-
-
-        // var currentLat = lat
-        // currentLatEl.textContent = currentLat
-
-        // var currentLon = lon
-        // currentLonEl.textContent = currentLon
 
         console.log(data)
 
@@ -97,62 +85,4 @@ function getApi() {
       });
   }
 
-// function getInputValue() {
-//   city.textContent = input
-// }
-
 getApi()
-
-//document.querySelectorAll("[data-foo='1']")
-//data.daily.length
-//var cardEl = document.getElementById(`data-id-${i}`)
-
-//cardEl.innerHTML = data.daily[i].temp
-// cardEl.innerHTML = data.daily[i].wind_speed
-// cardEl.innerHTML = data.daily[i].humidity
-// cardEl.innerHTML = data.daily[i].uvi
-// var weeklyTemp = data.current.temp
-// weeklyTempEl.textContent = weeklyTemp
-
-// var weeklyWind = data.current.wind_speed
-// weeklyWindEl.textContent = weeklyWind
-
-// var weeklyHum = data.current.humidity
-// weeklyHumEl.textContent = weeklyHum
-
-// var weeklyUvi = data.current.uvi;
-// weeklyUviEl.textContent = weeklyUvi
-
-// .then(function (response) {
-//   console.log(response.status);
-//   //  Conditional for the the response.status.
-//   if (response.status !== 200) {
-//     // Place the response.status on the page.
-//     console.log(response.status)
-//   }
-//   return response.json();
-// })
-// .then(function (data) {
-//   // Make sure to look at the response in the console and read how 404 response is structured.
-//   console.log(data);
-
-//   for (i = 0; i < 5; i++) {
-
-//       console.log(data.daily[i])
-//       // create new element
-//       var cardEl = document.createElement('div')
-//       // give the new element content
-//       cardEl.innerHTML = `
-//       <h2>This is the day</h2>
-//       <span>${data.daily[i].temp.day}</span>
-//       <span>${data.daily[i].wind_speed}</span>
-//       <span>${data.daily[i].humidity}</span>
-//       <span>${data.daily[i].uvi}</span>
-//       <img src="the address of the weather img" />
-//       `;
-
-//       // put the new element on the page
-//       weeklyWeatherContainer.append(cardEl)
-//   }
-// });
-// }
