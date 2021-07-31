06cd7cdbbe67ca399c8ce7fb05e8818ebe17c7c2
Wamackie <wamackie8456@gmail.com>
2021-07-31 11:37:47 -0400

    major update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var apiKey = '871609c38c37e30a4a9d514a96883f59'
 var city = 'portland'
 var input = document.getElementById('myInput')
+var test
 //current day DOM elements
 var currentTempEl = document.getElementById('current-temp')
 var currentWindEl = document.getElementById('current-wind')
@@ -13,9 +14,11 @@ var weeklyHumEl = document.getElementById('current-humidity')
 var weeklyUviEl = document.getElementById('current-uvi')
 
 var weeklyWeatherContainer = document.getElementById('weekly-weather')
+function getInputValue(){
+  var userInput = document.querySelector('#myInput').value
+  console.log(userInput)
 
-function getApi() {
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${apiKey}&units=imperial`
     fetch(apiUrl)
       .then(function (response) {
         console.log(response.status);
@@ -26,21 +29,22 @@ function getApi() {
         }
         return response.json();
       })
-      .then(function (data) {
-        console.log(data);
+      .then(function (data1) {
+        console.log(data1);
+        test = data1
 
-        var currentTemp = data.main.temp
+        var currentTemp = data1.main.temp
         currentTempEl.textContent = currentTemp
 
-        var currentWind = data.wind.speed
+        var currentWind = data1.wind.speed
         currentWindEl.textContent = currentWind
 
-        var currentHum = data.main.humidity
+        var currentHum = data1.main.humidity
         currentHumEl.textContent = currentHum
 
         function secondCall(lat,lon){
-          var lat = data.coord.lat
-          var lon = data.coord.lon
+          var lat = data1.coord.lat
+          var lon = data1.coord.lon
           var apiUrl2 = `https:api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
             fetch(apiUrl2)
             .then(function (response) {
@@ -52,37 +56,36 @@ function getApi() {
               }
               return response.json();
             })
-              .then(function (data) {
-                console.log(data);
-                var currentUvi = data.current.uvi
+              .then(function (data2) {
+                console.log(data2);
+                var currentUvi = data2.current.uvi
                 currentUviEl.textContent = currentUvi
 
-          console.log(lat,lon)
-        })
-        }
-        secondCall()
+          //console.log(lat,lon)
+          for (var i = 0; i < 5; i++) {
 
-        console.log(data)
-
-        for (i = 0; i < 5; i++) {
-
-            console.log(data.daily[i])
+            //console.log(data.daily[i])
+            console.log(data2.daily[i])
             // create new element
             var cardEl = document.createElement('div')
             // give the new element content
             cardEl.innerHTML = `
-            <h2>This is the day</h2>
-            <span>${data.daily[i].temp.day}</span>
-            <span>${data.daily[i].wind_speed}</span>
-            <span>${data.daily[i].humidity}</span>
-            <span>${data.current[i].uvi}</span>
+            <h2>${moment().add((i+1),'d').format('l')}</h2>
+            <span>${data2["daily"][i].temp.day}</span>
+            <span>${data2["daily"][i].wind_speed}</span>
+            <span>${data2["daily"][i].humidity}</span>
+            <span>${data2["daily"][i].uvi}</span>
             <img src="the address of the weather img" />
             `;
 
             // put the new element on the page
             weeklyWeatherContainer.append(cardEl)
         }
+        })
+        }
+        secondCall()
       });
   }
 
-getApi()
+
+
